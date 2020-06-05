d1867ecd527b4accb163fc84b8b106e8dc7f5efb
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-06-05 10:31:05 -0600

    Made string functions consistent
    All my string functions should respond the same for the same inputs. empty inputs return input as output. pass-thru behavior. Seeking for null values returns null because searching for a null is non-logical in strings.  \0 is different.

--- StringExtract.cs
+++ StringExtract.cs
@@ -19,27 +19,64 @@ namespace MySQLCLRFunctions
         private const int CHARACTER_AFTER_MARKER = 1;
         private const int BACKSET_FOR_ZEROBASED = -1;
 
+        /// <summary>
+        /// 
+        ///   IsNullWhiteSpaceOrEmpty - Simple expanding name to avoid constant confusion I have with what this function does.
+        ///   Because most people don't equivocate white space with an empty space, which is the opposite of space.  It is non-space.
+        ///   
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static bool IsNullOrWhiteSpaceOrEmpty(string input)
+        {
+            return (string.IsNullOrWhiteSpace(input));
+        }
+
+        /// <summary>
+        /// 
+        ///   LeftOf - Find the marker, and pull the entire string before that marker first appears, not including that marker.
+        /// 
+        /// </summary>
+        /// <param name="input">input possibly containing the marker, at least one instance of that marker.</param>
+        /// <param name="marker">the string to find.</param>
+        /// <usecase>null or empty string as a marker returns that input.  Think of use case: Running column values through a "LeftOf(',')"  Null input should not magically
+        /// change into a value, i.e., a set of blanks or empty string!  By returning input, we say, "Nevermind, just pass thru."</usecase>
+        /// <returns></returns>
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
         public static string LeftOf(string input, string marker)
         {
-            if (string.IsNullOrEmpty(input)) return null;
+            if (IsNullOrWhiteSpaceOrEmpty(input)) return input;
+            if (marker == null) return null;  // The test is invalid!  null is NOT a valid search value, so the result must represent INVALID parameter!
 
             var i = input.IndexOf(marker);
-            if (i == NOT_FOUND) return null;
+            if (i == NOT_FOUND) return string.Empty;  // Why?  So, "LeftOf('x', 'y') is '', because empty string represents NOTHING, where as null represents INVALID INPUTS. This may help chaining functions.  A null from a valid test will force any expression to null.  Is that what is desired?
+
             return input.Substring(0, i);
         }
         private static int IndexOfLastChar(this String str)
         {
-            if (string.IsNullOrWhiteSpace(str)) return NOT_FOUND;
+            if (IsNullOrWhiteSpaceOrEmpty(str)) return NOT_FOUND;
             return str.Length - 1;
         }
 
+        /// <summary>
+        /// 
+        ///   LeftOfNth - Find anything left of the nth found marker - TO THE PREVIOUS MARKER AND NOT INCLUDING THE PREVIOUS MARKER
+        ///   
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="marker"></param>
+        /// <param name="n"></param>
+        /// <flaws>May need a better name. LeftOfNthFromPrevious?</flaws>
+        /// <example>LeftOfNth("EDWPROD.UserData.x.y", ".", 2);=> UserData</example>
+        /// <example>LeftOfNth("EDWPROD.UserData.x.y", ".", 3);=> x</example>
+        /// <returns></returns>
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
         public static string LeftOfNth(string input, string marker, int n)
         {
-            if (string.IsNullOrEmpty(input)) return null;
-            if (n < 0) throw new ArgumentOutOfRangeException(nameof(n));
-            if (n == 0) return null;
+            if (string.IsNullOrEmpty(input)) return input;
+            if (n <= 0) throw new ArgumentOutOfRangeException(nameof(n));
             var i = input.IndexOf(marker);
             if (i == NOT_FOUND) return null;
             if (n == 1) return input.Substring(0, i);
@@ -49,7 +86,7 @@ namespace MySQLCLRFunctions
                 if (i == input.IndexOfLastChar()) return string.Empty;
                 previous_i = i;
                 i = input.IndexOf(marker, i + marker.Length);
-                if (i == NOT_FOUND) return null;
+                if (i == NOT_FOUND) return string.Empty;
             }
             if (i >= input.IndexOfLastChar()) return string.Empty;
             if (i == NOT_FOUND) return null;
@@ -57,6 +94,21 @@ namespace MySQLCLRFunctions
             return input.Substring(previous_i + marker.Length, seglen);
         }
 
+        /// <summary>
+        /// 
+        ///   LeftMOfNth - Pull m pieces back from the nth finding of a marker.
+        ///   
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="marker"></param>
+        /// <param name="n"></param>
+        /// <param name="howmany"></param>
+        /// <example>LeftMOfNth("EDWPROD.UserData.x.y", ".", 1, 1);=> EDWPROD</example>
+        /// <example>LeftMOfNth("EDWPROD.UserData.x.y", ".", 2, 2);=> EDWPROD.UserData</example>
+        /// <example>LeftMOfNth("EDWPROD.UserData.x.y", ".", 1, 2);=> </example>
+        /// <example>LeftMOfNth("EDWPROD.UserData.x.y", ".", 2, 4);=> </example>
+        /// <bug>LeftMOfNth("..", ".", 1, 1);=> .</bug>
+        /// <returns></returns>
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
         public static string LeftMOfNth(string input, string marker, int n, int howmany)
         {
@@ -71,7 +123,7 @@ namespace MySQLCLRFunctions
                 if (i >= input.IndexOfLastChar()) return string.Empty;
                 pointsfound[j+BACKSET_FOR_ZEROBASED] = i;
                 i = input.IndexOf(marker, i + marker.Length);
-                if (i == NOT_FOUND) return null;
+                if (i == NOT_FOUND) return string.Empty;
                 if (j == howmany)
                 {
                     int startofseg = pointsfound[j - howmany];
@@ -83,33 +135,41 @@ namespace MySQLCLRFunctions
             return string.Empty;
         }
 
+        /// <summary>
+        /// 
+        ///   LeftOfAny - Any of the characters in marker, any string before any of those.
+        ///   
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="markerchars"></param>
+        /// <returns></returns>
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
-        public static string LeftOfAny(string input, string markers)
+        public static string LeftOfAny(string input, string markerchars)
         {
-            if (string.IsNullOrEmpty(input)) return null;
+            if (IsNullOrWhiteSpaceOrEmpty(input)) return input;
 
-            var i = input.IndexOfAny(markers.ToCharArray());
-            if (i == NOT_FOUND) return null;
+            var i = input.IndexOfAny(markerchars.ToCharArray());
+            if (i == NOT_FOUND) return string.Empty;
             return input.Substring(0, i);
         }
 
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
         public static string RightOf(string input, string marker)
         {
-            if (string.IsNullOrEmpty(input)) return null;
+            if (string.IsNullOrEmpty(input)) return input;
 
             var i = input.IndexOf(marker);
-            if (i == NOT_FOUND) return null;
+            if (i == NOT_FOUND) return string.Empty;
             return input.Substring(i + marker.Length);
         }
 
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
         public static string RightOfAny(string input, string markers)
         {
-            if (string.IsNullOrEmpty(input)) return null;
+            if (IsNullOrWhiteSpaceOrEmpty(input)) return input;
 
             var i = input.IndexOfAny(markers.ToCharArray());
-            if (i == NOT_FOUND) return null;
+            if (i == NOT_FOUND) return string.Empty;
             return input.Substring(i + 1);
         }
 
@@ -121,9 +181,11 @@ namespace MySQLCLRFunctions
         /// <param name="FullName"></param>
         /// <returns></returns>
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
-        public static SqlString GetFirstName(SqlString FullName)
+        public static string GetFirstName(string FullName)
         {
-            string workingFullName = FullName.ToString().Trim();
+            if (IsNullOrWhiteSpaceOrEmpty(FullName)) return FullName;
+
+            string workingFullName = FullName.Trim();
             string firstName;
 
             if (workingFullName.EndsWith("(CWF)")) workingFullName = workingFullName.Substring(0, workingFullName.Length - 6).Trim();
@@ -148,7 +210,8 @@ namespace MySQLCLRFunctions
             {
                 firstName = firstName.Substring(0, firstName.Length - 2);
             }
-            return new SqlString(firstName.Trim());
+
+            return firstName.Trim();
         }
     }
 }
--- TestMySQLCLRFunctions/Program.cs
+++ TestMySQLCLRFunctions/Program.cs
@@ -12,39 +12,63 @@ namespace TestMySQLCLRFunctions
     {
         static void Main(string[] args)
         {
-            string output; string input; string marker; int markerno; int howmanyback;
-            //            var i = MySQLCLRFunctions.StringExtract.LeftOf("High.There", ".");
-            //            Debug.Print(i);
-            //            i = MySQLCLRFunctions.StringExtract.LeftOfNth("High.There", ".", 2);
-            //            Debug.Print(i);
-            input = "EDWPROD.UserData.x.y";
-            marker = ".";
-            markerno = 2;
-            howmanyback = 1;
-            output = MySQLCLRFunctions.StringExtract.LeftOfNth(input, marker, markerno);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfNth(\"{input}\", \"{marker}\", {markerno});=> {output}");
+            string output; string input; string marker; string markerchars; string markers; int markerno; int howmanyback;
+
+            input = "EDWPROD.UserData.x.y"; marker = "."; markerno = 2; howmanyback = 1;
+            output = MySQLCLRFunctions.StringExtract.LeftOfNth(input, marker, markerno); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfNth(\"{input}\", \"{marker}\", {markerno});=>{output}<=");
+            
             markerno = 3;
-            output = MySQLCLRFunctions.StringExtract.LeftOfNth(input, marker, markerno);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfNth(\"{input}\", \"{marker}\", {markerno});=> {output}");
+            output = MySQLCLRFunctions.StringExtract.LeftOfNth(input, marker, markerno); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfNth(\"{input}\", \"{marker}\", {markerno});=>{output}<=");
+            
             markerno = 5;
-            output = MySQLCLRFunctions.StringExtract.LeftOfNth(input, marker, markerno);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfNth(\"{input}\", \"{marker}\", {markerno});=> {output}");
+            output = MySQLCLRFunctions.StringExtract.LeftOfNth(input, marker, markerno); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfNth(\"{input}\", \"{marker}\", {markerno});=>{output}<=");
 
             markerno = 1;
-            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=> {output}");
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+            
             markerno = 2; howmanyback = 2;
-            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=> {output}");
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+ 
             markerno = 1; howmanyback = 2;
-            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=> {output}");
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+
             markerno = 2; howmanyback = 4;
-            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback);
-            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=> {output}");
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+
+            markerno = 1; howmanyback = 1; input = ".."; marker = ".";
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+
+            markerno = 1; howmanyback = 1; input = ".."; marker = "..";
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+
+            markerno = 2; howmanyback = 1; input = ".."; marker = ".";
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+
+            markerno = 2; howmanyback = 1; input = ".."; marker = ".";
+            output = MySQLCLRFunctions.StringExtract.LeftMOfNth(input, marker, markerno, howmanyback); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftMOfNth(\"{input}\", \"{marker}\", {markerno}, {howmanyback});=>{output}<=");
+
+            input = "He.was;therefore:Not."; markerchars = ".,;:";
+            output = MySQLCLRFunctions.StringExtract.LeftOfAny(input, markerchars); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfAny(\"{input}\", \"{markerchars}\";=>{output}<=");
+
+            input = "He.was;therefore:Not."; markerchars = ";:.,";
+            output = MySQLCLRFunctions.StringExtract.LeftOfAny(input, markerchars); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringExtract.LeftOfAny(\"{input}\", \"{markerchars}\";=>{output}<=");
+
             input = "[xxx]]y]";
-            output = MySQLCLRFunctions.StringTransform.RemoveSQLServerNameDelimiters($"{input}");
-            Debug.Print($"MySQLCLRFunctions.StringTransform.RemoveSQLServerNameDelimiters(\"{input}\");=> {output}");
+            output = MySQLCLRFunctions.StringTransform.RemoveSQLServerNameDelimiters($"{input}"); if (output == null) output = "{null}";
+            Debug.Print($"MySQLCLRFunctions.StringTransform.RemoveSQLServerNameDelimiters(\"{input}\");=>{output}<=");
         }
     }
 }
