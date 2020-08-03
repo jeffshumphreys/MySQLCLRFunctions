6d811224f0fef0b5be0172b28f229521424a9769
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-03 11:17:52 -0600

    Moved my experimental attribute into a shared area instead of being embedded in a class.

--- _SharedConstants.cs
+++ _SharedConstants.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace MySQLCLRFunctions
 {
@@ -9,5 +10,9 @@ namespace MySQLCLRFunctions
         internal const int UPSET_FOR_ZEROBASED = 1;
         internal const int UPSET_TO_ONEBASED_FROM_ZEROBASED = 1;
         internal const int ADJUST_POINTER_TO_INCLUDE = 1;  // So if the index of function returns a zero-based pointer, then to grab the value pointed at, we need to add one to include it.
+    }
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public class CountsAttribute : Attribute
+    {
     }
 }
