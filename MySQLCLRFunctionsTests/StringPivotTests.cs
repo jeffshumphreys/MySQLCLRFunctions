107572a345b4501969ee558db18f9c548e014c16
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-03 12:13:10 -0600

    Yet again.

---
+++ MySQLCLRFunctionsTests/StringPivotTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MySQLCLRFunctions.Tests
+{
+    [TestClass()]
+    public class StringPivotTests
+    {
+        [TestMethod()]
+        public void MatchesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PiecesWithContextTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PiecesTest()
+        {
+            var pieces = StringPivot.Pieces("This is input", "\b");
+            Assert.IsNotNull(pieces);
+        }
+
+        [Counts]
+        [TestMethod()]
+        public void PiecesTestCount()
+        {
+            var pieces = (string[])StringPivot.Pieces("This is input", " ");
+            Assert.AreEqual(expected: 3, pieces.Length);
+        }
+
+        [TestMethod()]
+        public void PiecesWithMatchesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetWordsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SplitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void KeyValuePairsWithMultiValuesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CapturesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ MySQLCLRFunctionsTests/StringTransformTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySQLCLRFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLCLRFunctions.Tests
+{
+    [TestClass()]
+    public class StringTransformTests
+    {
+        [TestMethod()]
+        public void ReplaceMatchTest()
+        {
+            var input = "ThisIsIt";
+            var validoutput = "Th!s!s!t";
+            var output = StringTransform.ReplaceMatch(input, "[it]", "!");
+            Assert.AreEqual(expected: validoutput, output);
+        }
+
+        [TestMethod()]
+        public void ReplaceRecursiveTest()
+        {
+            var input = "This is                a test  of the     emergency  ";
+            var validoutput = "This is a test of the emergency ";
+            var output = StringTransform.ReplaceRecursive(input, "  ", " ");
+            Assert.AreEqual(expected: validoutput, output);
+        }
+    }
+}
