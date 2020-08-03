a6aa58d25f9bd84684b9882edcfcff4f38c14c6b
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-03 11:47:58 -0600

    Trying like fuck to get the "CORRECT" test project into the main solution!

---
+++ MySQLCLRFunctionsTests3/StringDecodeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySQLCLRFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLCLRFunctions.Tests
+{
+    [TestClass()]
+    public class StringDecodeTests
+    {
+        [TestMethod()]
+        public void RevealNonPrintablesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
