4baadf0eaf89e7631fbf890d629023c51c15a0d5
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-05 17:39:42 -0600

    Again a rename, so I don't quite remember what's in here.

---
+++ StringReduceCustomizations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SqlServer.Server;
+
+namespace MySQLCLRFunctions
+{
+    public static class StringReduceCustomizations
+    {
+        /***************************************************************************************************************************************************************************************************
+         * 
+         * StripDownCherwellDescription    Descriptions in Cherwell tickets are often full of garbage.  Strip out garbage.
+         * Needs work.
+         * 
+         **************************************************************************************************************************************************************************************/
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static string StripDownCherwellDescription(string input)
+        {
+            if (StringTest.IsNullOrWhiteSpaceOrEmpty(input)) return input;
+
+            return input.Trim();
+        }
+    }
+}
