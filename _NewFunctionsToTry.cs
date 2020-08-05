a186f849bb32238dbb901e6a8cc2da6c98acc1b7
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-05 17:33:17 -0600

    Need to test better performance options and streaming around SqlChar

--- _NewFunctionsToTry.cs
+++ _NewFunctionsToTry.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -21,5 +23,20 @@ namespace MySQLCLRFunctions
             Thread.Sleep(wait);
             return x + y;
         }
+
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static SqlString TestSqlChar(SqlChars streamingString)
+        {
+            //char c = streamingString.Read(1,)
+            return streamingString.ToSqlString();
+        }
+
+        /*
+         * Function to SIMPLIFY adding missing dlls.
+         * Warning: The Microsoft .NET Framework assembly 'microsoft.visualstudio.qualitytools.unittestframework, version=10.0.0.0, culture=neutral, publickeytoken=b03f5f7f11d50a3a, processorarchitecture=msil.' you are registering is not fully tested in the SQL Server hosted environment and is not supported. In the future, if you upgrade or service this assembly or the .NET Framework, your CLR integration routine may stop working. Please refer SQL Server Books Online for more details.
+         * Error: CANNOT Assembly 'MySQLCLRFunctions' references assembly 'system.web, version=2.0.0.0, culture=neutral, publickeytoken=b03f5f7f11d50a3a.', which is not present in the current database. SQL Server attempted to locate and automatically load the referenced assembly from the same location where referring assembly came from, but that operation has failed (reason: 2(The system cannot find the file specified.)). Please load the referenced assembly into the current database and retry your request.
+         * 
+         * Found a site with a guy who showed how to do it in 1,392 steps!
+         */
     }
 }
