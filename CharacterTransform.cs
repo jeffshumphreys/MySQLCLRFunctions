dd56503f4c5e2bb7ce0a99176c4443a6a69fd6b6
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-05 13:43:11 -0600

    Decided it would be a good idea to keep character logic out of string logic, at least as best as can be done.

---
+++ CharacterTransform.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MySQLCLRFunctions.StringTest;
+using static MySQLCLRFunctions._SharedConstants;
+
+namespace MySQLCLRFunctions
+{
+    public static class CharacterTransform
+    {
+        public static string ReplaceFirstC(this string input, char replacement)
+        {
+            if (IsNullOrEmpty(input)) return input;
+
+            //if (Char.GetUnicodeCategory(replacement) != System.Globalization.UnicodeCategory.) Warning!
+
+            char firstC = input.First();
+            if (firstC == replacement) return input;
+            return StringReduce.LTrimOne(input) + replacement;
+        }
+
+        public static string ReplaceLastC(this string input, char replacement)
+        {
+            if (IsNullOrEmpty(input)) return input;
+
+            //if (Char.GetUnicodeCategory(replacement) != System.Globalization.UnicodeCategory.) Warning!
+
+            char lastC = input.Last();
+            if (lastC == replacement) return input;
+            return StringReduce.RTrimOne(input) + replacement;
+        }
+    }
+}
