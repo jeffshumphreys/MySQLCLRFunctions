cd7523ea750fe42206724795f077ab5c70f603ad
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-05 13:44:02 -0600

    Attempted to get rid of C#8.0 warnings, and some other warning messages that had been reviewed and found incorrectible.

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "<Pending>", Scope = "member", Target = "~M:MySQLCLRFunctions.StringReduce.TrimBracketing(System.String)~System.String")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "<Pending>", Scope = "member", Target = "~M:MySQLCLRFunctions.CharacterTransform.ReplaceFirstC(System.String,System.Char)~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:MySQLCLRFunctions._NewFunctionsToTry")]
