3269361cfff8e5d7ecacccf75e4d322ddd16bb01
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-05 17:35:41 -0600

    Didn't get very far except a header for what aggregates I want.

---
+++ StringAggregate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLCLRFunctions
+{
+    public static class StringAggregate
+    {
+        // Product
+        // Median
+        // with "AND"  so "X, Y, and Z"  or "OR"
+
+    }
+}
