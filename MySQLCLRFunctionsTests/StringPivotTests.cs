c6769a951cd8e6a7ee44195ec407caf38b7754a3
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-10 16:18:44 -0600

    Had to recall the LINQ syntax for testing the IEnumerable returns; may still be wrong since it's unordered.

--- MySQLCLRFunctionsTests/StringPivotTests.cs
+++ MySQLCLRFunctionsTests/StringPivotTests.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static MySQLCLRFunctions.StringPivot;
 
 namespace MySQLCLRFunctions.Tests
 {
@@ -17,7 +21,7 @@ namespace MySQLCLRFunctions.Tests
         }
 
         [Fact]
-        public void PiecesTest()
+        public void PiecesXTest()
         {
             var pieces = StringPivot.PiecesX("This is input", "\b");
             Assert.NotNull(pieces);
@@ -25,16 +29,28 @@ namespace MySQLCLRFunctions.Tests
 
         [Counts]
         [Fact]
-        public void PiecesTestCount()
+        public void PiecesXTestCount()
         {
             var pieces = (string[])StringPivot.PiecesX("This is input", " ");
             Assert.Equal(expected: 3, pieces.Length);
         }
 
+        [PositiveTest]
         [Fact]
-        public void PiecesWithMatchesTest()
+        public void PiecesXTestFirst()
         {
-            Assert.False(true);
+            var pieces = (string[])StringPivot.PiecesX("This is input", " ");
+            Assert.Equal(expected: "This", pieces[0]);
+        }
+
+        [Fact]
+        public void PiecesWithMatchesXTest()
+        {
+            var input = "Joan Joyce Julia June Karen Katherine (Kathy) Laura Louise Marilyn Mary Moira Molly Monica Nancy Natalie Norma Pamela (Pam) Patricia (Pat) Paula Ruth Sally Sarah Sophia Susan (Sue) Teresa (Terry) Valerie Veronica Vivian Vickie Wanda Wilma Yvonne";
+            IEnumerable<PiecesWithMatchesRecord> pieces = (IEnumerable<PiecesWithMatchesRecord>)PiecesWithMatchesX(input, @"(\([^)]+\)|\w+)");
+
+             foreach (PiecesWithMatchesRecord piece in pieces.Take(1))
+                Assert.Equal(expected: "Joan", actual: piece.piece);
         }
 
         [Fact]
