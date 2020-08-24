6f44129cfe461997eca3c57d956897eca3154e1e
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-24 14:23:13 -0600

    Some cleanup and adding some stubs for future functions, and adding the TrimNormalizeStringInput function to actually help my real code.

--- StringReduceCustomizations.cs
+++ StringReduceCustomizations.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.SqlServer.Server;
+using static MySQLCLRFunctions.StringTest;
 
 namespace MySQLCLRFunctions
 {
+    /*
+     * Customizations means functions that are not as general as the ones in StringReduce.
+     * RemoveFunctionCall may be a tad general, but still only applies to code.
+     * 
+     */
     public static class StringReduceCustomizations
     {
         /***************************************************************************************************************************************************************************************************
@@ -14,9 +20,35 @@ namespace MySQLCLRFunctions
         [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
         public static string TrimCherwellTicketField(string input)
         {
-            if (StringTest.IsNullOrWhiteSpaceOrEmpty(input)) return input;
+            if (IsNullOrWhiteSpaceOrEmpty(input)) return input;
 
             return input.Trim();
+        }
+
+        /**
+         * Converts "TRIM(NULLIF(mgrdtl1.info, ''))" to "TRIM(mgrdtl1.info)"
+         */
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static string RemoveFunctionCall(string input, string functionName)
+        {
+            // scan line for functionname + "(", then find matching ")" and work backwards to previous ","
+            return input;
+        }
+
+        /**************************************************************************************************************************************************************************************
+         * 
+         * Replaces my overused function spread:  NULLIF(LTRIM(RTRIM(CAST(c.Title AS VARCHAR(100)))), '')
+         * Except for the downcast from NVARCHAR to VARCHAR, which is technically risky, but if all input is ascii...
+         * 
+         **************************************************************************************************************************************************************************************/
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static string TrimNormalizeStringInput(string input)
+        {
+            if (IsNull(input)) return input; // Null is a fine value.
+            if (IsEmptyOrWhiteSpace(input)) return null; // Normalize to null for joins, coalesces, ISNULL tests
+
+            input = input.Trim().REPLACEMATCHX(@"\s+", " ");
+            return input;
         }
     }
 }
