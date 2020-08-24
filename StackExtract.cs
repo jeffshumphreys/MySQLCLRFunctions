f65bdca4ae35babaab49c7abdc244aac56629159
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-24 14:25:00 -0600

    This is hopeless.  I don't think I can access the SQL Server T-SQL stack from here.

---
+++ StackExtract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+
+namespace MySQLCLRFunctions
+{
+    public static class StackExtract
+    {
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static string Caller()
+        {
+            return string.Empty;
+        }
+    }
+}
