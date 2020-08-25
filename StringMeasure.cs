52f40092433c1561a6c94e829ad28bb54810ac2b
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-25 12:43:05 -0600

    Added some more measures.  measuring characters in a string is kind of hybrid.

--- StringMeasure.cs
+++ StringMeasure.cs
@@ -4,11 +4,19 @@ using System.Text.RegularExpressions;
 using System.Data.SqlTypes;
 using static MySQLCLRFunctions.StringTest;
 using static MySQLCLRFunctions._SharedConstants;
+using System.Linq;
 
 /*
 * Take measurements from a string.  Not actual values from a string as Extract does, and not a "changed" (immutable) string like Transform or Pivot, or Format, Reduce.
 * 
 * WARNING SqlDateTime is not the same as datetime.  SqlDateTime maps to DATETIME, DateTime maps to DATETIME2
+* 
+* S = string
+* C = char
+* X = Regex pattern
+* M
+* N
+* L = Array, list.
 */
 namespace MySQLCLRFunctions
 {
@@ -32,6 +40,29 @@ namespace MySQLCLRFunctions
             return howMany;
         }
 
+        [SqlFunction(DataAccess = DataAccessKind.None, IsDeterministic = true, IsPrecise = true)]
+        public static SqlInt32 HowManyC(string input, char c)
+        {
+            if (IsNull(input))  return SqlInt32.Null;
+            if (IsEmpty(input)) return 0;
+
+            int howMany = input.Count(i => i == c);
+
+            return howMany;
+        }
+
+        internal static int? HOWMANYS(this string input, string marker)
+        {
+            var rtnval = HowManyS(input, marker);
+            if (rtnval.IsNull) return null;
+            return rtnval.Value;
+        }
+        internal static int? HOWMANYC(this SqlString input, char c)
+        {
+            var rtnval = HowManyC(input.ToString(), c);   // Not very clever.
+            if (rtnval.IsNull) return null;
+            return rtnval.Value;
+        }
         /***************************************************************************************************************************************************************************************************
          * 
          * How many of a string occurs?  What about overlap?
@@ -49,6 +80,41 @@ namespace MySQLCLRFunctions
             MatchCollection matches = Regex.Matches(input, pattern, RegexOptions.None, matchTimeout: TimeSpan.FromSeconds(2));
 
             return matches.Count;
+        }
+        internal static int? HOWMANYX(this string input, string pattern)
+        {
+            var rtnval = HowManyX(input, pattern);
+            if (rtnval.IsNull) return null;
+            return rtnval.Value;
+        }
+        internal static int? HOWMANYX(this SqlString input, string pattern)
+        {
+            var rtnval = HowManyX(input.ToString(), pattern);
+            if (rtnval.IsNull) return null;
+            return rtnval.Value;
+        }
+
+        /***************************************************************************************************************************************************************************************************
+         * 
+         * Same as SQL Server IN clause.
+         * 
+         ***************************************************************************************************************************************************************************************************/
+        public static bool? In(this string input, params string[] markers)
+        {
+            return input.InL(markers);
+        }
+
+        public static bool? InL(this string input, string[] markers)
+        {
+            if (IsNull(input)) return null;
+            if (IsEmpty(input)) return false;    // Something can never be in nothing. Can empty = empty? No.
+
+            foreach (string marker in markers)
+            {
+                if (marker != null && input == marker) return true;
+            }
+
+            return false;
         }
         /***************************************************************************************************************************************************************************************************
          * 
