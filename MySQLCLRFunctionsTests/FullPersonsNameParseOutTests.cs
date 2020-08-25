5ef473b93996c44e8cd2ca2dc51043c6fc88a90d
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-25 12:39:26 -0600

    Big stuff.  I want to clean up all my full name strings to first last.

---
+++ MySQLCLRFunctionsTests/FullPersonsNameParseOutTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MySQLCLRFunctions.StringCastIntoStruct;
+
+namespace MySQLCLRFunctions.Tests
+{
+    public class FullPersonsNameParseOutTests : IDisposable
+    {
+        FullPersonsNameParseOut parser;
+
+
+        public FullPersonsNameParseOutTests()
+        {
+            parser = FullPersonsNameParseOut.Parse("Jeff H");
+        }
+
+        [Fact]
+        public void FirstNameMatches()
+        {
+             Assert.Equal(actual: parser.GivenName, expected: "Jeff");
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
