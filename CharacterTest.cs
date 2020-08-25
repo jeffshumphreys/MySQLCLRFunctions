abd147d62675c2f1e4742fcc51c581488b1f4933
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-08-25 12:45:52 -0600

    Really expanding character testing.  Converted "Empty" tests to "Whitespace" tests.

--- CharacterTest.cs
+++ CharacterTest.cs
@@ -2,6 +2,9 @@
 using System.Text.RegularExpressions;
 using static MySQLCLRFunctions._SharedConstants;
 using static MySQLCLRFunctions.StringTest;
+using static MySQLCLRFunctions.CharacterTransform;
+using static MySQLCLRFunctions.CharacterTest;
+using System.Linq;
 
 namespace MySQLCLRFunctions
 {
@@ -20,5 +23,29 @@ namespace MySQLCLRFunctions
         {
             return NotInX(input, pattern);
         }
+
+        public static bool ISNULLORWHITESPACE(this char? input)
+        {
+            if (input == null) return true;
+            if (input.In(' ', '\n', '\r', '\t')) return true;    // Wait: Are these empty or whitespace?
+            if (input == 0) return true; // Tricky.  It's not really SQL NULL.
+            return false;
+        }
+
+        public static bool ISNULLORWHITESPACE(this char input)
+        {
+            if (input == ' ') return true;
+            if (input == 0) return true; // Tricky.  It's not really SQL NULL.
+            return false;
+        }
+        public static bool In(this char input, params char[] markers)
+        {
+            return markers.Contains(input);
+        }
+        public static bool In(this char? input, params char[] markers)
+        {
+            return markers.Contains((char)input);
+        }
+        public static bool IsWhiteSpace(char c) { return c == ' '; } // Uhh, will this work from Sql Server??
     }
 }
