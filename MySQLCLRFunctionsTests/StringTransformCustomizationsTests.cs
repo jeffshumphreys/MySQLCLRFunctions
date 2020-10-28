e2e7e9507531496bb2802ddef38b4fcf3706ccb3
jeffshumphreys@gmail.com <jeffrey.humphreys@simplot.com>
2020-10-28 09:15:28 -0600

    A variety of tests of odd formed inputs.

--- MySQLCLRFunctionsTests/StringTransformCustomizationsTests.cs
+++ MySQLCLRFunctionsTests/StringTransformCustomizationsTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using MySQLCLRFunctions;
+using Xunit;
 using static MySQLCLRFunctions.StringTransformCustomizations;
 
 namespace MySQLCLRFunctions.Tests
@@ -27,6 +28,21 @@ namespace MySQLCLRFunctions.Tests
         public void TrimSQLTest()
         {
             Assert.True(false, "This test needs an implementation");
+        }
+
+        [Theory]
+        [InlineData("TableName", "[TableName]")]
+        [InlineData("Table Name", "[Table Name]")]
+        [InlineData("A.B.C.D", "[A].[B].[C].[D]")]
+        [InlineData("A.B.C", "[A].[B].[C]")]
+        [InlineData("A.B", "[A].[B]")]
+        [InlineData("A..B", "[A]..[B]")]
+        [InlineData("ETLSteps.[0001_Jobs_0001_CMR_AJF]", "[ETLSteps].[0001_Jobs_0001_CMR_AJF]")]
+        [InlineData("ETLSteps.[0001_Jobs_0001_CMR_AJF", "[ETLSteps].[0001_Jobs_0001_CMR_AJF]")]
+        public void CleanUpSQLServerNameDelimitersTest(string input, string validoutput)
+        {
+            var output = CleanUpSQLServerNameDelimiters(input);
+            Assert.Equal(expected: validoutput, output);
         }
     }
 }
